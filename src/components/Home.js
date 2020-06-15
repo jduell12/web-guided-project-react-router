964b2799b72dddf7e72ed6bb4a9efa6e4e4d8d1d
Jessica <duellje@oregonstate.edu>
2020-06-15 13:27:22 -0400

    completed task 6

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,16 @@
 import React from 'react'
 // We'll need React Router's own version of the History API
+import { useHistory } from 'react-router-dom'
 
 export default function Home() {
   // 👉 STEP 5 - Build a click handler that will imperatively
   // navigate us to <website base URL>/items-list
+  const history = useHistory(); //react router replacement for the native JS history in the browser
 
   const routeToShop = () => {
-
+    //handle navigation with code 
+    //can't do history.pushState(null, null, '/') bc react components wouldn't notice the change
+    history.push('/items-list');
   }
 
   return (
